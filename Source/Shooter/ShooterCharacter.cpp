49f0c1e284c76eb55938aef1d421e78b9b2369e6
Diliup Gabadamudalige <diliupg@gmail.com>
2022-03-31 07:36:16 +0530

    L17 complete. Mouse turn look up/down OK. Previous mouse turn did not wok as I had a space in the axis binding string name!

--- Source/Shooter/ShooterCharacter.cpp
+++ Source/Shooter/ShooterCharacter.cpp
@@ -6,7 +6,9 @@
 #include "Camera/CameraComponent.h"
 
 // Sets default values
-AShooterCharacter::AShooterCharacter()
+AShooterCharacter::AShooterCharacter( ) :
+	BaseTurnRate( 45.f ),
+	BaseLookUpRate( 45.f )
 {
  	// Set this character to call Tick() every frame.  You can turn this off to improve performance if you don't need it.
 	PrimaryActorTick.bCanEverTick = true;
@@ -60,6 +62,17 @@ void AShooterCharacter::MoveRight( float Value )
 	}
 }
 
+void AShooterCharacter::TurnAtRate( float Rate )
+{
+	// calculate delta for this frame from the rate information
+	AddControllerYawInput( Rate * BaseTurnRate * GetWorld( )->GetDeltaSeconds( ) ); // deg/sec * sec/frame = deg/frame
+}
+
+void AShooterCharacter::LookUpAtRate( float Rate )
+{
+	AddControllerPitchInput( Rate * BaseLookUpRate * GetWorld( )->GetDeltaSeconds( ) ); // deg/sec * sec/frame = deg/frame
+}
+
 // Called every frame
 void AShooterCharacter::Tick(float DeltaTime)
 {
@@ -75,5 +88,7 @@ void AShooterCharacter::SetupPlayerInputComponent(UInputComponent* PlayerInputCo
 
 	PlayerInputComponent->BindAxis( "MoveForward", this, &AShooterCharacter::MoveForward );
 	PlayerInputComponent->BindAxis( "MoveRight", this, &AShooterCharacter::MoveRight );
+	PlayerInputComponent->BindAxis( "TurnRate", this, &AShooterCharacter::TurnAtRate );
+	PlayerInputComponent->BindAxis( "LookUpRate", this, &AShooterCharacter::LookUpAtRate );
 }
 
--- Source/Shooter/ShooterCharacter.h
+++ Source/Shooter/ShooterCharacter.h
@@ -25,6 +25,17 @@ protected:
 	/* called for side to side input*/
 	void MoveRight( float Value );
 
+	/*
+	called via input to turn at given rate.
+	@param Rate  This is a normalized rate i.e. 1.0 means 100% of desired turn rate
+	*/
+	void TurnAtRate( float Rate );
+
+	/*
+	called via input to look up/down at given rate.
+	@param Rate  This is a normalized rate i.e. 1.0 means 100% of desired rate
+	*/
+	void LookUpAtRate( float Rate );
 public:	
 	// Called every frame
 	virtual void Tick(float DeltaTime) override;
@@ -41,6 +52,14 @@ private:
 	/* camera that follows the character*/
 	UPROPERTY( VisibleAnywhere, BlueprintReadOnly, Category = Camera, meta = ( AllowPrivateAccess = "true" ) )
 	class UCameraComponent* FollowCamera;
+
+	/* Base turn rate in deg/sec.  Other scaling may affect final turn rate */
+	UPROPERTY(VisibleAnywhere, BlueprintReadOnly, Category = Camera, meta = ( AllowPrivateAccess = "true" ) )
+	float BaseTurnRate;
+
+	/* Base look up/down rate in deg/sec.  Other scaling may affect final turn rate */
+	UPROPERTY( VisibleAnywhere, BlueprintReadOnly, Category = Camera, meta = ( AllowPrivateAccess = "true" ) )
+	float BaseLookUpRate;
 
 public:
 	/** returns CameraBoom subObject (child object) */
