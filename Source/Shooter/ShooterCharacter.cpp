89e17782185ca572cbb19b0b02f74a71817ded41
Diliup Gabadamudalige <diliupg@gmail.com>
2022-04-05 19:21:44 +0530

    L51 complete. Crosshair aim factor done.

--- Source/Shooter/ShooterCharacter.cpp
+++ Source/Shooter/ShooterCharacter.cpp
@@ -32,7 +32,14 @@ AShooterCharacter::AShooterCharacter( ) :
 	CameraDefaultFOV( 0.f ), // set in BeginPlay
 	CameraZoomedFOV( 35.f ),
 	CameraCurrentFOV( 0.f ),
-	ZoomInterpSpeed( 20.f )
+	ZoomInterpSpeed( 20.f ),
+	// crosshair spread factors
+	CrosshairSpreadMultiplier( 0.f ),
+	CrosshairVelocityFactor( 0.f ),
+	CrosshairInAirFactor( 0.f ),
+	CrosshairAimFactor( 0.f ),
+	CrosshairShootingFactor( 0.f )
+
 {
 	// Set this character to call Tick() every frame.  You can turn this off to improve performance if you don't need it.
 	PrimaryActorTick.bCanEverTick = true;
@@ -329,10 +336,31 @@ void AShooterCharacter::CalculateCrosshairSpread( float DeltaTime )
 			30.f );
 	}
 
+	// Calculate crosshair aim factor
+	if ( bAiming ) // are we aiming?
+	{
+		// Shrink crosshairs a small amount very quickly
+		CrosshairAimFactor = FMath::FInterpTo(
+			CrosshairAimFactor,
+			0.6f,
+			DeltaTime,
+			30.f );
+	}
+	else // not aiming
+	{
+		// spread crosshairs back to normal very quickly
+		CrosshairAimFactor = FMath::FInterpTo(
+			CrosshairAimFactor,
+			0.f,
+			DeltaTime,
+			30.f );
+	}
+
 	CrosshairSpreadMultiplier = 
 		0.5f + 
 		CrosshairVelocityFactor + 
-		CrosshairInAirFactor;
+		CrosshairInAirFactor -
+		CrosshairAimFactor;
 }
 
 // Called every frame
